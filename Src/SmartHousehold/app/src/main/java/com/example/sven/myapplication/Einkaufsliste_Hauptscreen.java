85e4ee427a4900fd92701f66d6f238d769902b13
SvenGuthe <sven_guthe@web.de>
2016-02-26 13:37:49 +0100

    Android

---
+++ Src/SmartHousehold/app/src/main/java/com/example/sven/myapplication/Einkaufsliste_Hauptscreen.java
@@ -0,0 +1,13 @@
+package com.example.sven.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Einkaufsliste_Hauptscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_einkaufsliste__hauptscreen);
+    }
+}
